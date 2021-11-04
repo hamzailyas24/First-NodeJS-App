b965e5cc6d9647104bd3d2176dffe17ef1428f92
Hamza Ilyas <hamza24061@gmail.com>
2021-11-05 02:57:49 +0500

    Get and Post Complete

---
+++ app.js
@@ -0,0 +1,46 @@
+const responseDiv = document.getElementById("response-div");
+
+function getUsers() {
+  const URL = "https://hamzailyas-nodejs.herokuapp.com/users";
+
+  axios.get(URL).then((response) => {
+    const users = response.data;
+    console.log(users);
+
+    if (response.data.length === 0) {
+      //   alert("No Users");
+      responseDiv.innerHTML = "No Users";
+    } else {
+      const usersList = users.map((user) => {
+        return `<tr><td>${user.name}</td><td>${user.email}</td><td>${user.address}</td></tr>`;
+      });
+
+      const resultDiv = document.getElementById("result-div");
+
+      resultDiv.innerHTML = usersList.join("");
+    }
+  });
+}
+
+function addUser() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+  const addUserURL = "https://hamzailyas-nodejs.herokuapp.com/user";
+
+  if (name === "" || email === "" || address === "") {
+    alert("Please Fill All the Fields");
+  } else {
+    const userData = {
+      name: name,
+      email: email,
+      address: address,
+    };
+
+    axios.post(addUserURL, userData).then((response) => {
+      //   alert("User Added");
+      responseDiv.innerHTML = "User Added";
+      location.reload();
+    });
+  }
+}
